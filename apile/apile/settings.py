c97ba57111f7365fb1b9ff515946f344999f0580
Sam LoCascio <sflocascio@gmail.com>
2018-11-28 11:00:33 -0500

    add postgresql database to project

--- apile/apile/settings.py
+++ apile/apile/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'apile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apile',
+        'USER': 'apile',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
